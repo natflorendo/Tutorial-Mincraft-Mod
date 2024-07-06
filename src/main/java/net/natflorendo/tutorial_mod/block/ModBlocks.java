503eb12d75858b363f36edbcdc480d90aba8eff7
Nathan Florendo <nathan.florendo1@gmail.com>
2024-07-05 19:23:56 -0500

    Added loot drops and ores

--- src/main/java/net/natflorendo/tutorial_mod/block/ModBlocks.java
+++ src/main/java/net/natflorendo/tutorial_mod/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.natflorendo.tutorial_mod.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -41,4 +43,25 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+//    Ores
+    public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 6),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE = registerBlock("deepslate_sapphire_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 7),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE)
+                            .strength(5f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> NETHER_SAPPHIRE_ORE = registerBlock("nether_sapphire_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 7),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERRACK)
+                            .strength(1f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 7),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.END_STONE)
+                            .strength(3f).requiresCorrectToolForDrops()));
 }
